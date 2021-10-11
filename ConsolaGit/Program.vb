e63a9842c43f90eebdfb8957c1a1e5bac213f4b4
cecilioAlvarez <cmac@arquitecturajava.com>
2021-10-11 12:36:25 +0200

    he creado un programa para hacer una media

--- ConsolaGit/Program.vb
+++ ConsolaGit/Program.vb
@@ -2,7 +2,36 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+
+        Dim numero1 As Decimal
+        Dim numero2 As Decimal
+        Dim numero3 As Decimal
+        Dim numero4 As Decimal
+
+        Console.WriteLine("por favor introduce la nota1")
+
+        numero1 = Console.ReadLine
+
+
+        Console.WriteLine("por favor introduce la nota2")
+
+        numero2 = Console.ReadLine
+
+        Console.WriteLine("por favor introduce la nota3")
+
+        numero3 = Console.ReadLine
+
+        Console.WriteLine("por favor introduce la nota4")
+
+        numero4 = Console.ReadLine
+
+        Dim suma As Decimal
+
+        suma = numero1 + numero2 + numero3 + numero4
+
+        Console.WriteLine(suma / 4)
+
+
 
     End Sub
 End Module
