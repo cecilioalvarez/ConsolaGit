324f79dc1963ffdac316fa46af23423097301092
cecilioAlvarez <cmac@arquitecturajava.com>
2021-10-11 13:05:57 +0200

    solucion tabla del 7

--- ConsolaGit/Program.vb
+++ ConsolaGit/Program.vb
@@ -5,6 +5,15 @@ Module Program
 
 
         ' hay que sacar por la consola la tabla de multiplicar del 7
+        Dim i = 1
+        While i <= 10
+            'Console.WriteLine(i)
+            Dim multiplicacion = i * 7
+            Console.WriteLine("7" & "x" & i & "=" & multiplicacion)
+            ' incrementas el bucle en 1 y pasas a la siguiente iteracion
+            i = i + 1
+
+        End While
 
 
 
