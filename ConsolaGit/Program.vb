dabb7b67359f35db096e8d93bb47980ac2ef421c
cecilioAlvarez <cmac@arquitecturajava.com>
2021-10-11 14:23:36 +0200

    do loop

--- ConsolaGit/Program.vb
+++ ConsolaGit/Program.vb
@@ -3,15 +3,28 @@ Imports System
 Module Program
     Sub Main(args As String())
 
-        Dim i As Integer = 0
 
-        While i <= 10
-            Console.WriteLine(i * 2 + 1)
-            i = i + 1
+        For i = 0 To 10 Step 2
+            'incremento de 1 en 1 por defecto
+            Console.WriteLine("hola")
+
+        Next
+        Dim j As Integer
+
+        While j < 10
+            Console.WriteLine(j)
+            j = j + 1
 
         End While
+        Dim k As Integer = 0
 
-
+        k = 11
+        ' el bucle do while 
+        Do
+            'bucle se ejecuta una vez al menos
+            Console.WriteLine("hola do while")
+            k = k + 1
+        Loop Until k > 10
 
 
     End Sub
