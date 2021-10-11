76c5926121c714bce1f0579584cd1211d143bcf9
cecilioAlvarez <cmac@arquitecturajava.com>
2021-10-11 12:43:30 +0200

    ejemplo repaso whle

--- ConsolaGit/Program.vb
+++ ConsolaGit/Program.vb
@@ -3,33 +3,15 @@ Imports System
 Module Program
     Sub Main(args As String())
 
-        Dim numero1 As Decimal
-        Dim numero2 As Decimal
-        Dim numero3 As Decimal
-        Dim numero4 As Decimal
 
-        Console.WriteLine("por favor introduce la nota1")
+        Dim i As Integer = 0
 
-        numero1 = Console.ReadLine
+        While i < 5
+            Console.WriteLine(i)
 
+            i = i + 1
+        End While
 
-        Console.WriteLine("por favor introduce la nota2")
-
-        numero2 = Console.ReadLine
-
-        Console.WriteLine("por favor introduce la nota3")
-
-        numero3 = Console.ReadLine
-
-        Console.WriteLine("por favor introduce la nota4")
-
-        numero4 = Console.ReadLine
-
-        Dim suma As Decimal
-
-        suma = numero1 + numero2 + numero3 + numero4
-
-        Console.WriteLine(suma / 4)
 
 
 
