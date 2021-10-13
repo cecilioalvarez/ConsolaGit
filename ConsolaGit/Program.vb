f808b8d3cfbb9f1401ac77ffd8785514e7b3a5e8
cecilioAlvarez <cmac@arquitecturajava.com>
2021-10-13 12:32:51 +0200

    calculo meida

--- ConsolaGit/Program.vb
+++ ConsolaGit/Program.vb
@@ -11,25 +11,16 @@ Module Program
         lista(2) = 20
         lista(3) = 10
         lista(4) = 3
-
-
-        ' una me va almacenando cual es el numero que por ahora 
-        'es el mayor
-        Dim mayor As Integer = 0
-        'la otra me almacena la posicion en la que se encuentra
-        Dim posicion = 0
+        Dim total As Decimal
 
         For i = 0 To lista.Length - 1
-
-            If (lista(i) > mayor) Then
-                mayor = lista(i)
-                posicion = i
-
-            End If
-
+            total = total + lista(i)
         Next
+        Dim media = total / lista.Length
+        Console.WriteLine(media)
 
-        Console.WriteLine(posicion)
+
+
 
     End Sub
 End Module
