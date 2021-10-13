f4fbe939dec2ac2d478743a82ff0c07909ab5b10
cecilioAlvarez <cmac@arquitecturajava.com>
2021-10-13 10:32:03 +0200

    mayor

--- ConsolaGit/Program.vb
+++ ConsolaGit/Program.vb
@@ -4,7 +4,7 @@ Module Program
     Sub Main(args As String())
 
 
-        Dim lista(5) As Integer
+        Dim lista(4) As Integer
 
         lista(0) = 2
         lista(1) = 25
@@ -12,16 +12,20 @@ Module Program
         lista(3) = 10
         lista(4) = 3
 
-        Console.WriteLine(lista(0))
-        Console.WriteLine(lista(1))
-        Console.WriteLine(lista(2))
-        Console.WriteLine(lista(3))
-        Console.WriteLine(lista(4))
 
 
+        Dim numero As Integer = 0
 
+        For i = 0 To lista.Length - 1
 
+            If (numero < lista(i)) Then
 
+                numero = lista(i)
+            End If
+
+        Next
+
+        Console.WriteLine(numero)
 
     End Sub
 End Module
